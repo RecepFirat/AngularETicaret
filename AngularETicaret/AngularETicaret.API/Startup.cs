7aacaae4aee0357ca6da746e61684621ef3040b3
recep tayyip fırat <rtf3444@gmail.com>
2021-07-13 01:06:55 +0300

    İlk Bölüm bitirildi

--- AngularETicaret/AngularETicaret.API/Startup.cs
+++ AngularETicaret/AngularETicaret.API/Startup.cs
@@ -1,15 +1,12 @@
+
+using AngularETicaret.Data.DataContext;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AngularETicaret.API
 {
@@ -25,15 +22,21 @@ namespace AngularETicaret.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddDbContext<StoreContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDbContext<StoreContext>(opt =>
+            //{
+            //    opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), conf =>
+            //    {
+            //        conf.MigrationsAssembly("AngularETicaret.API");
+            //    });
+            //});
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AngularETicaret.API", Version = "v1" });
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
---
+++ AngularETicaret/AngularETicaret.Data/DataContext/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using AngularETicaret.Data.DBModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularETicaret.Data.DataContext
+{
+    public class StoreContext:DbContext
+    {
+        public StoreContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
